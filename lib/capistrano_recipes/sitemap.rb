f43aaa1cb0d81d5865ff915941caea16a7a7ad36
Nubis <nubis@woobiz.com.ar>
2013-07-06 20:30:38 -0300

    updated deploy task

--- lib/capistrano_recipes/sitemap.rb
+++ lib/capistrano_recipes/sitemap.rb
@@ -1,7 +1,7 @@
 namespace :sitemap do
   desc "Copy the sitemap from the latest release"
   task :symlink, roles: :app do
-    run "cp -r #{previous_release}/public/sitemap* #{release_path}/public/"
+    run "if [[ -d #{previous_release}/public ]]; then cp -fr #{previous_release}/public/sitemap* #{release_path}/public/; fi"
   end
   after "deploy:finalize_update", "sitemap:symlink"
 end
