e6dbdd8344b2c6d2695b4a5e6faec3d7d65ba504
Tomas Rojas <tmsrjs@gmail.com>
2014-02-21 15:29:45 -0300

    use single square brackets for testing public directory existence

--- lib/capistrano_recipes/sitemap.rb
+++ lib/capistrano_recipes/sitemap.rb
@@ -1,7 +1,7 @@
 namespace :sitemap do
   desc "Copy the sitemap from the latest release"
   task :symlink, roles: :app do
-    run "if [[ -d #{previous_release}/public ]]; then cp -fr #{previous_release}/public/sitemap* #{release_path}/public/; fi"
+    run "if [ -d #{previous_release}/public ]; then cp -fr #{previous_release}/public/sitemap* #{release_path}/public/; fi"
   end
   after "deploy:finalize_update", "sitemap:symlink"
 end
